66d16dcb26e6800216a346b4a969e7f2b10fe609
Matheus <matheusabreu297@gmail.com>
2019-06-16 18:21:30 -0300

    Conexao de dados

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Main from '../template/Main'
+
+
+export default props =>
+<Main icon="home" title="Inicio"
+subtitle="Projeto 2">
+    <div className="display-4">Bem Vindo!</div>
+    <hr/>
+    <p className="mb-0">Sistema para exemplificar a contruçãod e um cadastro desenvolvido em React</p>
+</Main> 
